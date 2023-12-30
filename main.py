9c41ead6dd4cfcfde4e28cd02f5bbdec2ce16bc1
mbgoodguy <nerooren4001@gmail.com>
2023-12-31 01:15:28 +0300

    Add functions for work with ws

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import string
+from pprint import pprint
+
+import requests
 import gspread
 import os
 
 from dotenv import load_dotenv
-from gspread import Client, Spreadsheet, worksheet
+from gspread import Client, Spreadsheet, worksheet, Worksheet
+from gspread.utils import rowcol_to_a1
 
 load_dotenv()
 
@@ -26,6 +31,21 @@ def show_main_worksheet(sh: Spreadsheet):
     return main_ws
 
 
+def update_ws_color(sh: Spreadsheet):
+    ws = show_main_worksheet(sh)
+    ws.update_tab_color('#d400ff')
+
+
+def insert_some_data(ws: Worksheet):
+    ws.insert_rows([
+        list(range(1, 10)),
+        list(string.ascii_lowercase),
+        list(string.ascii_uppercase),
+        list(string.punctuation),
+        list("ABOBA"),
+    ])
+
+
 def create_ws_fill_and_delete(sh: Spreadsheet):
     another_ws = sh.add_worksheet('another', rows=3, cols=3)
     print(another_ws)
@@ -37,22 +57,65 @@ def create_ws_fill_and_delete(sh: Spreadsheet):
     sh.del_worksheet(another_ws)
 
 
-def update_ws_color(sh: Spreadsheet):
-    ws = show_main_worksheet(sh)
-    ws.update_tab_color('#d400ff')
+# def update_table_by_cells(ws: Worksheet):
+#     ws.update_cell(row=1, col=1, value='ABOBA')
+#     ws.update_cell(row=2, col=1, value='AMOGUS')
+
+
+def update_table_by_cells(ws: Worksheet):
+    rows = 3
+    cols = 3
+    row = 4
+    col = 2
+    range_start = rowcol_to_a1(row, col)
+    range_end = rowcol_to_a1(row + rows - 1, col + cols - 1)
+    cells_range = f'{range_start}:{range_end}'
+    print('update range: ', cells_range)
+    values = [['a'] * cols] * rows
+    print("values", values)
+    ws.update(cells_range, values)
+
+
+def show_all_values_in_ws(ws: Worksheet):
+    list_of_lists = ws.get_all_values()  # получаем все значения, даже те, в которых отсутствует значение. Будет '' в пустой ячейке
+    pprint(list_of_lists)
+
+
+def create_and_fill_comments_with_ws(sh: Spreadsheet):
+    comments_data = requests.get('https://jsonplaceholder.typicode.com/comments').json()
+    header_row = ["postId", "id", "name", "email", "body"]
+    rows = [header_row]
+    for comment in comments_data:  # type: dict
+        rows.append([
+            comment.get(key, '')
+            for key in header_row
+        ])
+    comment_ws = sh.add_worksheet("comments", rows=1, cols=len(header_row))
+    comment_ws.insert_rows(rows)
+
+
+def show_ws(ws: Worksheet):
+    list_of_dicts = ws.get_all_records()
+    pprint(list_of_dicts)  # для вывода большого кол-ва данных
 
 
 def main():
     gc: Client = gspread.service_account('./gspreadlearn-06870fa5f3a0.json')
     sh: Spreadsheet = gc.open_by_url(table_url)
-    # print(sh)
-    # print(type(table_id))
-    # print(sh.sheet1.get('A1:A2'))
+    ws = sh.sheet1
+    # ws2 = sh.worksheet('comments')
 
-    show_available_worksheets(sh)
-    show_main_worksheet(sh)
-    update_ws_color(sh)
-    create_ws_fill_and_delete(sh)
+
+    # show_available_worksheets(sh)
+    # show_main_worksheet(sh)
+    # update_ws_color(sh)
+    # create_ws_fill_and_delete(sh)
+    # insert_some_data(ws)
+    # update_table_by_cells(ws)
+    # show_all_values_in_ws(ws)
+    create_and_fill_comments_with_ws(sh)
+    # comments = sh.worksheet('comments')
+    # show_ws(ws2)  # вывод спсика со словарями в более удобном чем в print формате
 
 
 if __name__ == '__main__':
